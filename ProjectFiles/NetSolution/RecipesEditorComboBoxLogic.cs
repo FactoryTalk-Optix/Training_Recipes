61b3b8c7e29015d8207a150f0c2062d686b29962
asemlucben <>
2023-11-10 15:04:23 +0000

    Update README.md

---
+++ ProjectFiles/NetSolution/RecipesEditorComboBoxLogic.cs
@@ -0,0 +1,59 @@
+#region Using directives
+using System;
+using FTOptix.CoreBase;
+using FTOptix.HMIProject;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.NetLogic;
+using FTOptix.OPCUAServer;
+using FTOptix.Recipe;
+using FTOptix.UI;
+using FTOptix.Store;
+using FTOptix.SQLiteStore;
+#endregion
+
+public class RecipesEditorComboBoxLogic : BaseNetLogic
+{
+	public override void Start()
+	{
+		comboBox = (ComboBox)Owner;
+		comboBox.SelectedValueVariable.VariableChange += SelectedValueVariable_VariableChange;
+		LoadSelectedRecipeData();
+	}
+
+	private void SelectedValueVariable_VariableChange(object sender, VariableChangeEventArgs e)
+	{
+		LoadSelectedRecipeData();
+	}
+
+	private void LoadSelectedRecipeData()
+    {
+		LocalizedText selectedText = (LocalizedText)comboBox.SelectedValue;
+		if (selectedText == null || string.IsNullOrEmpty(selectedText.Text))
+			return;
+
+		var recipeSchemaEditor = Owner.Owner;
+		var recipeSchemaVariable = recipeSchemaEditor.GetVariable("RecipeSchema");
+		if (recipeSchemaVariable == null)
+			return;
+
+		var recipeSchemaNodeId = (NodeId)recipeSchemaVariable.Value.Value;
+
+		var recipeSchemaObject = (RecipeSchema)InformationModel.Get(recipeSchemaNodeId);
+		if (recipeSchemaObject == null)
+			return;
+
+		var editModelNode = recipeSchemaObject.GetObject("EditModel");
+		if (editModelNode == null)
+			return;
+
+		recipeSchemaObject.CopyFromStoreRecipe(comboBox.Text, editModelNode.NodeId, CopyErrorPolicy.BestEffortCopy);
+	}
+
+	public override void Stop()
+	{
+		comboBox.SelectedValueVariable.VariableChange -= SelectedValueVariable_VariableChange;
+	}
+
+	private ComboBox comboBox;
+}
